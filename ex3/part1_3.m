3c8cf9f93e10a867cb60f76628f6f42094f357b1
Dongho Kang <east0822@gmail.com>
2017-03-26 14:17:02 +0200

    part1-3 finished?

--- ex3/part1_3.m
+++ ex3/part1_3.m
@@ -7,30 +7,58 @@ clear all
 addpath(genpath('files/TASK1'))
 
 % parameters
+off_files = {'bun', 'cat'};
+
+sigmas = {100};
 iter = 5;
 
 % debug
 debug = false;
-sigma = 100;
 
-%% DATA LOAD
+%% PART 1-3
 disp('===================================================================')
 disp('PART 1-3')
 
-disp('-------------------------------------------------------------------')
-disp('data loading...')
+for file_idx = 1:size(off_files, 2)
+    %% DATA LOAD
+    file = off_files{file_idx};
+    
+    disp('-------------------------------------------------------------------')
+    disp(['off file loading: ', file, '.off'])
+    
+    % TODO update readOFF file
+    %  V  #V by 3 list of vertices
+    %  F  #F by 3 list of triangle indices
+    %  N  #V by 3 list of normals
+    
+    [V_file,F_file,~,~,~] = readOFF([file, '.off']);
+    N_file = per_vertex_normals(V_file,F_file);
+    
+    %% CALCULATE NEW V
+    disp('calculating new V matrix...')
 
-% TODO update readOFF file
-%  V  #V by 3 list of vertices
-%  F  #F by 3 list of triangle indices
-%  N  #V by 3 list of normals
+    for i=1:size(sigmas, 2)
+        
+        sigma = sigmas{i}; 
+        disp(['for sigma = ', num2str(sigma)])
+        
+        V = V_file;
+        N = N_file;
+        
+        % X is initial V
+        X = V;
+        
+        for j=1:iter
+            % calculate V_next: v_next = v - f(x) grad f(x)
+            V_next = V - FxGradFx3D(X, V, N, sigma, debug);
+            
+            % V update
+            V = V_next;
+        end
 
-[V,F,~,~,~] = readOFF('bun.off');
-N = per_vertex_normals(V,F);
-V_next = V;
-
-for i=1:iter
-    V_next = V - FxGradFx3D(V, V_next, N, sigma, debug);
+        filename = [file, '_sig', num2str(sigma), '_it', num2str(iter), '.off'];
+        
+        disp(['off file saving: ', filename])
+        writeOFF(filename, V, F_file);
+    end
 end
-
-writeOFF(filename, V_next,F)
