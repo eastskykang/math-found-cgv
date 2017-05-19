657d50b3379ad5d3979992a89e54a1e8eec62955
Dongho Kang <east0822@gmail.com>
2017-05-19 16:48:23 +0200

    ex6 started

---
+++ ex6/part2.m
@@ -0,0 +1,20 @@
+%% EXERCISE X - PART X
+clc 
+close all 
+clear all 
+
+% paths
+addpath('')
+
+% parameters
+
+% debug
+debug = false;
+
+%% DATA LOAD 
+disp('===================================================================')
+disp('data loading...')
+
+disp('-------------------------------------------------------------------')
+
+load('')
