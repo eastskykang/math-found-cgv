f1adf591230dfbd63a2b44dcdaa268c44d55b843
Dongho Kang <east0822@gmail.com>
2017-05-22 16:13:08 +0200

    ex6 task2 implementing

--- ex6/part2.m
+++ ex6/part2.m
@@ -18,16 +18,15 @@ disp('data loading...')
 
 disp('-------------------------------------------------------------------')
 
-% load('')
-
-
 if debug 
     
-    % params
+    % figure idx
+    fig_idx = 1;
+    
+    % options & params
+    max_iter = 100;
     color_hist_implemented = false;
-    
-    lambda = 0.0075;
-    
+        
     % get image (batman)
     I = imread('Part 2 - Interactive Segmentation/batman.jpg');
     
@@ -38,12 +37,13 @@ if debug
     load('Part 2 - Interactive Segmentation/scribbles2.mat');
     
     % show image with scribbles
-    figure(1)
+    figure(fig_idx)
     imshow(I)
     hold on
     plot(seed_fg(:, 1), seed_fg(:, 2), '.r') % foreground
     plot(seed_bg(:, 1), seed_bg(:, 2), '.b') % background
     hold off
+    fig_idx = fig_idx + 1;
     
     % get color hist
     if ~color_hist_implemented
@@ -61,9 +61,49 @@ if debug
     
     f_i = fi(hist_fg, hist_bg, I);
     
+    % primal dual algorithm 
+    % initialization
+    sigma = 0.35;
+    tau = 0.35;
+    lambda = 0.0075;
+    theta = 1;
     
-  
+    % (x_0, y_0) in X x Y
+    x = double(I);
+    y = div(x, [h, w]);
     
+    % x_bar_0 = x_0
+    x_bar = x;
+    
+    % iteration 
+    for i=1:max_iter 
+        % TODO: termination condition
+        % TODO: shape check
+        
+        % y_n+1
+        grad_xn_bar = grad(x_bar, [h, w]);  % grad(x_bar_n)
+        
+        y = (y + sigma * grad_xn_bar) ...
+            ./ max(1, norm2(y + sigma * grad_xn_bar));
+        
+        % x_n+1
+        div_y = div(y, [h, w]); % div(y_n+1)
+        x_n = x;                % x before update (save temporary for x_bar)
+        
+        f = fi(hist_fg, hist_bf, I);
+        x = min(1, max(0, x - tau * (- div_y) + tau * f));
+        
+        % x_bar_n+1
+        x_bar = x + theta * (x - x_n);
+    end
+    
+    % result of iteration 
+    x = reshape(x, [h, w]);
+    
+    figure(fig_idx)
+    imshow(uint8(x))
+    fig_idx = fig_idx + 1;
+
 end
 
 function [f_i] = fi(hist_fg, hist_bg, I)
@@ -85,4 +125,37 @@ function [f_i] = fi(hist_fg, hist_bg, I)
     idx = sub2ind(size(hist_fg), r_bin, g_bin, b_bin);
     
     f_i = log (hist_bg(idx)) - log (hist_fg(idx));
+end
+
+function [grad_x, grad_y] = grad(f, size)
+    
+    % input f is vectorized
+    f = reshape(f, size);
+    
+    % Neumann boundary condition 
+    f = padarray(f, [1, 1], 'replicate', 'post');
+    
+    % get forward differences
+    grad_x = diff(f, 1, 2);
+    grad_y = diff(f, 1, 1);
+    
+    if size(f) ~= size
+        error('grad error: size does not match')
+    end
+end
+
+function [div_f] = div(f, size)
+    
+    % input f is vectorized
+    f = reshape(f, size);
+    
+    % Neumann boundary condition 
+    f = padarray(f, [1, 1], 'replicate', 'pre');
+    
+    % get backward difference 
+    div_f = diff(f, 1, 2) + diff(f, 1, 1); 
+       
+    if size(f) ~= size
+        error('grad error: size does not match')
+    end
 end
